289f8429d7b3357a27502e2c6234798b7596387a
501 <501@academicos.cesde.edu.co>
2020-02-20 08:45:46 -0500

    imagenes añadidas

--- App.js
+++ App.js
@@ -4,19 +4,20 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={{flex:1  }}>
-      <View style={{flex:1, backgroundColor: "cyan"}}>
+      <View style={{flex:3, backgroundColor: "cyan"}}>
         <Text>cyan</Text>
-        <Image></Image>
+        <Image source={{uri: 'https://i.ytimg.com/vi/68ugkg9RePc/hqdefault.jpg'}} style={{width: "80%", height: "70%"}}/>
 
       </View>
-      <View style={{flex:2, backgroundColor: "blue"}}>
+      <View style={{flex:3, backgroundColor: "blue"}}>
         <Text>blue</Text>
+        <Image source={require('./assets/ahaha.jpg')} style={{width: "80%", height: "70%"}}/>
         
       </View>
-      <View style={{flex:2, backgroundColor: "powderblue"}}>
+      <View style={{flex:1, backgroundColor: "powderblue"}}>
         <Text>powderblue</Text>
       </View>
-      <View style={{flex:2, backgroundColor: "grey"}}>
+      <View style={{flex:1, backgroundColor: "grey"}}>
         <Text>grey</Text>
       </View>
       
