877b2be9364df5a3d8c3aa7fbc7526e5ffa59856
andreabarreraudea123 <jazulado1337@hotmail.com>
2020-02-27 08:46:56 -0500

    class

--- App.js
+++ App.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{flex:1  }}>
-      <View style={{flex:3, backgroundColor: "cyan"}}>
-        <Text>cyan</Text>
-        <Image source={{uri: 'https://i.ytimg.com/vi/68ugkg9RePc/hqdefault.jpg'}} style={{width: "80%", height: "70%"}}/>
-
-      </View>
-      <View style={{flex:3, backgroundColor: "blue"}}>
-        <Text>blue</Text>
-        <Image source={require('./assets/ahaha.jpg')} style={{width: "80%", height: "70%"}}/>
+export default class hw extends Component {
+  constructor() {
+    super();
+    this.state ={ 
+      nombre : '',
+      identificacion : '',
+      salario : ''
+    }
+  }
+  render(){
+    return (
+      <View style={{ flex:1 }}>
+        <View style={{flex:3, backgroundColor: "cyan"}}>
+          <Text>cyan</Text>
+          <Image source={{uri: 'https://i.ytimg.com/vi/68ugkg9RePc/hqdefault.jpg'}} style={{width: "80%", height: "70%"}}/>
+  
+        </View>
+        <View style={{flex:3, backgroundColor: "blue"}}>
+          <Text>blue</Text>
+          <TextInput 
+          placeholder="Nombre" 
+          style={{backgroundColor: "white", height:30, width: 100}} 
+          value={this.state.nombre}
+          onChangeText={(nombre) =>  this.setState({nombre})}
+          />
+          <TextInput 
+          placeholder="identificacion" 
+          style={{backgroundColor: "white", height:30, width: 100}} 
+          value={this.state.identificacion}
+          onChangeText={(identificacion) =>  this.setState({identificacion})}
+          />
+          <TextInput 
+          placeholder="salario" 
+          style={{backgroundColor: "white", height:30, width: 100}} 
+          value={this.state.salario}
+          onChangeText={(salario) =>  this.setState({salario})}
+          />
+          <Button 
+          title="Enviar"
+          onPress={() => {alert(this.state.nombre)}}
+          />
+          <Image source={require('./assets/ahaha.jpg')} style={{width: "80%", height: "70%"}}/>
+          
+        </View>
+        <View style={{flex:1, backgroundColor: "powderblue"}}>
+          <Text>powderblue</Text>
+        </View>
+        <View style={{flex:1, backgroundColor: "grey"}}>
+          <Text>grey</Text>
+        </View>
         
       </View>
-      <View style={{flex:1, backgroundColor: "powderblue"}}>
-        <Text>powderblue</Text>
-      </View>
-      <View style={{flex:1, backgroundColor: "grey"}}>
-        <Text>grey</Text>
-      </View>
-      
-    </View>
-  );
+    );
+
+  }
 }
 
 const styles = StyleSheet.create({
